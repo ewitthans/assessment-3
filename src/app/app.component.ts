e162abe06136b0a72b3f82674a2e64b9da345141
Eric Witthans <homesteadlaptop@Erics-MBP.attlocal.net>
2019-06-23 23:20:52 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 import { APIService } from "./api.service";
 import { Stock } from "./stock";
-import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   @Input()
   public stockData: Stock;
+  public search = "";
+
+  addStock(search: string) {
+    this.search = search;
+    console.log(search);
+
+    this.svc.getStocks().subscribe(data => {
+      this.stockData = data;
+      //console.log(this.stockData);
+      //console.log(this.stockData.companyName);
+    });
+  }
 
   constructor(private svc: APIService) {}
 
-  ngOnInit() {
-    this.svc.getStocks().subscribe(data => {
-      this.stockData = data;
-      console.log(this.stockData);
-      console.log(this.stockData.companyName);
-    });
-  }
+  ngOnInit() {}
 }
